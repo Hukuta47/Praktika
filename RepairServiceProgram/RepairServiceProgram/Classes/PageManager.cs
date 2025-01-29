a48af8e5b2c88a3131dfe771fa7f57f6f784d9d7
Никита <hukuta47@yandex.ru>
2025-01-29 15:39:02 +0900

    ы

---
+++ RepairServiceProgram/RepairServiceProgram/Classes/PageManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using System.Windows.Controls;
+
+namespace RepairServiceProgram.Classes
+{
+    public static class PageManager
+    {
+        static Frame MainFrame;
+
+
+    }
+}
