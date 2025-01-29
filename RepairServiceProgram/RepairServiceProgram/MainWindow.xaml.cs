23d0aed2a3120729fe53b4313b9f70eff44ce1d4
Никита <hukuta47@yandex.ru>
2025-01-29 10:44:45 +0900

    ура, работает

--- RepairServiceProgram/RepairServiceProgram/MainWindow.xaml.cs
+++ RepairServiceProgram/RepairServiceProgram/MainWindow.xaml.cs
@@ -1,21 +1,28 @@
-﻿using System.Windows;
-using RepairServiceProgram.Dialogs;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace RepairServiceProgram
 {
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void WindowLoaded(object sender, RoutedEventArgs e)
-        {
-            if (new LogInDialog().ShowDialog() == true)
-            {
-
-            }
         }
     }
 }
