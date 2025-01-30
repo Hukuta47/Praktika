e6b7bc737aa9576a70439283fe8865ee3410aca0
Никита <hukuta47@yandex.ru>
2025-01-30 17:20:23 +0900

    Update PageRegister.xaml.cs

--- RepairServiceProgram/RepairServiceProgram/Pages/PageRegister.xaml.cs
+++ RepairServiceProgram/RepairServiceProgram/Pages/PageRegister.xaml.cs
@@ -1,8 +1,5 @@
 ﻿using RepairServiceProgram.Classes;
-using RepairServiceProgram.DataDB;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -21,23 +18,63 @@ namespace RepairServiceProgram.Pages
         }
         private void Register_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(Textbox_FullNameData.Text))
+
+
+            //string Fullname = Textbox_FullNameData.Text;
+            //string FirstName = Fullname.Split(' ')[0];
+            //string LastName = Fullname.Split(' ')[1];
+            //string Patronymic = Fullname.Split(' ')[2];
+            //string Username = Textbox_UsernameData.Text;
+            //string NumberPhone = Textbox_NumberPhoneData.Text;
+
+            //string FirstPassword = Textbox_PasswordData.Text;
+            //string SecondPassword = Textbox_RePasswordData.Text;
+
+
+            List<TextBox> textBoxs = new List<TextBox>()
             {
-                Textblock_ErrFullNameData.Visibility = Visibility.Visible;
-                Textblock_ErrFullNameData.Text = "Поле пустрое";
+                Textbox_FullNameData,
+                Textbox_NumberPhoneData,
+                Textbox_UsernameData,
+                Textbox_PasswordData,
+                Textbox_RePasswordData
+            };
+            List<TextBlock> textBlocks = new List<TextBlock>()
+            {
+                Textblock_ErrFullNameData,
+                Textblock_ErrNumberPhoneData,
+                Textblock_ErrUsernameData,
+                Textblock_ErrPasswordData,
+                Textblock_ErrRePasswordData
+            };
+            for (int i = 0; i < textBoxs.Count; i++)
+            {
+                if (string.IsNullOrWhiteSpace(textBoxs[i].Text))
+                {
+                    textBlocks[i].Visibility = Visibility.Visible;
+                    textBlocks[i].Text = "Бля ты еблан?";
+                }
+                else
+                {
+                    textBlocks[i].Visibility = Visibility.Hidden;
+                    textBlocks[i].Text = "";
+
+                    string Fullname = Textbox_FullNameData.Text;
+                    string FirstName = Fullname.Split(' ')[0];
+                    string LastName = Fullname.Split(' ')[1];
+                    string Patronymic = Fullname.Split(' ')[2];
+                    string Username = Textbox_UsernameData.Text;
+                    string NumberPhone = Textbox_NumberPhoneData.Text;
+
+                    string FirstPassword = Textbox_PasswordData.Text;
+                    string SecondPassword = Textbox_RePasswordData.Text;
+
+
+
+
+                }
+                
             }
-
-            string Fullname = Textbox_FullNameData.Text;
-            string FirstName = Fullname.Split(' ')[0];
-            string LastName = Fullname.Split(' ')[1];
-            string Patronymic = Fullname.Split(' ')[2];
-            string Username = Textbox_UsernameData.Text;
-            string NumberPhone = Textbox_NumberPhoneData.Text;
-
-            string FirstPassword = Textbox_PasswordData.Text;
-            string SecondPassword = Textbox_RePasswordData.Text;
-
-
 
 
 
@@ -61,62 +98,6 @@ namespace RepairServiceProgram.Pages
             };
             */
 
-        }
-
-        private void HideErrorMessages()
-        {
-            Textblock_ErrFullNameData.Visibility = Visibility.Hidden;
-            Textblock_ErrUsernameData.Visibility = Visibility.Hidden;
-            Textblock_ErrNumberPhoneData.Visibility = Visibility.Hidden;
-            Textblock_ErrPasswordData.Visibility = Visibility.Hidden;
-            Textblock_ErrRePasswordData.Visibility = Visibility.Hidden;
-        }
-
-        private void ShowErrorMessage(TextBlock textBlock, string message)
-        {
-            textBlock.Text = message;
-            textBlock.Visibility = Visibility.Visible;
-        }
-
-        private bool IsUserExists(string username)
-        {
-            return MainWindow.ModelDB.LoginData.Any(data => data.Username == username);
-        }
-        bool CheckIsNullOrEmptyFields()
-        {
-            List<TextBox> textBoxs = new List<TextBox>()
-            {
-                Textbox_FullNameData,
-                Textbox_UsernameData,
-                Textbox_NumberPhoneData,
-                Textbox_PasswordData,
-                Textbox_RePasswordData
-            };
-            List<TextBlock> textBlocks = new List<TextBlock>()
-            {
-                Textblock_ErrFullNameData,
-                Textblock_ErrUsernameData,
-                Textblock_ErrNumberPhoneData,
-                Textblock_ErrPasswordData,
-                Textblock_ErrRePasswordData
-            };
-            for (int i = 0; i < textBoxs.Count; i++)
-            {
-                if (string.IsNullOrEmpty(textBoxs[i].Text))
-                {
-                    textBlocks[i].Text = "Поле пустрое";
-                    textBlocks[i].Visibility = Visibility.Visible;
-                }
-                else
-                {
-                    textBlocks[i].Text = "";
-                    textBlocks[i].Visibility = Visibility.Hidden;
-                }
-            }
-        }
-        private bool IsPasswordCorrect(string username, string password)
-        {
-            return MainWindow.ModelDB.LoginData.Any(data => data.Username == username && data.Password == password);
         }
 
 
