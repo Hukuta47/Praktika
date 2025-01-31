9c38bb373c56759a9f777774448b30712fa83ee2
Никита <hukuta47@yandex.ru>
2025-01-31 16:22:15 +0900

    Update PageRegister.xaml.cs
    уцра

--- RepairServiceProgram/RepairServiceProgram/Pages/PageRegister.xaml.cs
+++ RepairServiceProgram/RepairServiceProgram/Pages/PageRegister.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using RepairServiceProgram.Classes;
-using System.Collections.Generic;
+using RepairServiceProgram.DataDB;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -18,7 +19,110 @@ namespace RepairServiceProgram.Pages
         }
         private void Register_Click(object sender, RoutedEventArgs e)
         {
+            HideErrorMessages();
 
+
+            string firstName = Textbox_FullNameData.Text.Split(' ')[0];
+            string lastName = Textbox_FullNameData.Text.Split(' ')[0];
+            string patronymic = Textbox_FullNameData.Text.Split(' ')[0];
+            string username = Textbox_UsernameData.Text;
+            string phoneNumber = Textbox_NumberPhoneData.Text;
+            string password = Textbox_PasswordData.Text;
+            string rePassword = Textbox_RePasswordData.Text;
+
+            if (string.IsNullOrEmpty(Textbox_FullNameData.Text))
+            {
+                ShowErrorMessage(Textblock_ErrFullNameData, "Поле пустое");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(username))
+            {
+                ShowErrorMessage(Textblock_ErrUsernameData, "Поле пустое");
+                return;
+            }
+
+            if (IsUsernameExists(username))
+            {
+                ShowErrorMessage(Textblock_ErrUsernameData, "Такое имя пользователя уже существует");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(phoneNumber))
+            {
+                ShowErrorMessage(Textblock_ErrNumberPhoneData, "Поле пустое");
+                return;
+            }
+
+            if (IsPhoneNumberExists(phoneNumber))
+            {
+                ShowErrorMessage(Textblock_ErrNumberPhoneData, "Такой номер телефона уже существует");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(password))
+            {
+                ShowErrorMessage(Textblock_ErrPasswordData, "Поле пустое");
+                return;
+            }
+
+            if (password != rePassword)
+            {
+                ShowErrorMessage(Textblock_ErrRePasswordData, "Пароли не совпадают");
+                return;
+            }
+
+           
+            LoginData loginData = new LoginData()
+            {
+                Username = username,
+                Password = rePassword,
+            };
+
+            MainWindow.ModelDB.LoginData.Add(loginData);
+            MainWindow.ModelDB.SaveChanges();
+
+            Users user = new Users()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Patronymic = patronymic,
+                Phone = phoneNumber,
+                LoginDataID = MainWindow.ModelDB.LoginData.FirstOrDefault(n => n.Username == username).LoginDataID,
+                UserTypeID = 4
+            };
+
+            MainWindow.ModelDB.Users.Add(user);
+            MainWindow.ModelDB.SaveChanges();
+
+
+            Textblock_SuccsesRegister.Visibility = Visibility.Visible;
+        }
+
+
+        private void HideErrorMessages()
+        {
+            Textblock_ErrFullNameData.Visibility = Visibility.Hidden;
+            Textblock_ErrUsernameData.Visibility = Visibility.Hidden;
+            Textblock_ErrNumberPhoneData.Visibility = Visibility.Hidden;
+            Textblock_ErrPasswordData.Visibility = Visibility.Hidden;
+            Textblock_ErrRePasswordData.Visibility = Visibility.Hidden;
+        }
+
+        private void ShowErrorMessage(TextBlock textBlock, string message)
+        {
+            textBlock.Text = message;
+            textBlock.Visibility = Visibility.Visible;
+        }
+
+        private bool IsUsernameExists(string username)
+        {
+            return MainWindow.ModelDB.LoginData.Any(data => data.Username == username);
+        }
+
+        private bool IsPhoneNumberExists(string phoneNumber)
+        {
+            return MainWindow.ModelDB.Users.Any(user => user.Phone == phoneNumber);
         }
 
 
